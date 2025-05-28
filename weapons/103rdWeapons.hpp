f7b7a16c3c14318bf845a37f13b7dc79b8b8e2a7
Gigasus <33152835+Gigasus3@users.noreply.github.com>
2025-05-28 17:04:52 +1000

    Weapons

---
+++ weapons/103rdWeapons.hpp
@@ -0,0 +1,19 @@
+class 103rd_M7S_Gigasus: 103rd_M7_SMG
+{
+    scope=2;
+    scopeArsenal=2;
+    author="103rd ODST AUX";
+	model="MA_Weapons\data\MA_M7_SMG.p3d";
+    displayName="[103rd] M7S SMG (Gigasus)";
+    baseWeapon = "103rd_M7S_Gigasus";
+    hiddenSelections[]=
+    {
+    "M7_Body",
+	"M7_Addons"
+    };
+    hiddenSelectionsTextures[]=
+    {
+    "OD103\data\weapons\M7\Gigasus_M7_SMG_Body_CO.paa",
+	"OD103\data\weapons\M7\Gigasus_M7_SMG_Addons_CO.paa"
+    };
+};

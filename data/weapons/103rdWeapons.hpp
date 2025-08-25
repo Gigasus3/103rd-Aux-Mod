f6f801d12845454be0bdee7a697dfa28e3d79d3c
Gigasus <33152835+Gigasus3@users.noreply.github.com>
2025-08-26 00:22:50 +1000

    Updated Code

--- data/weapons/103rdWeapons.hpp
+++ data/weapons/103rdWeapons.hpp
@@ -17,3 +17,129 @@ class 103rd_M7S_Gigasus: 103rd_M7_SMG
 	"OD103\data\weapons\M7\Gigasus_M7_SMG_Addons_CO.paa"
     };
 };
+
+class 103rd_M7S: 103rd_M7_SMG
+{
+	scope=2;
+	scopeArsenal=2;
+	author="103rd ODST AUX";
+	model="MA_Weapons\data\MA_M7_SMG.p3d";
+	displayName="[103rd] M7S SMG";
+    baseWeapon = "103rd_M7S";
+	picture="\MA_Weapons\data\M7_SMG\Data\Icons\M7_Icon.paa";
+	hiddenSelections[]=
+	{
+		"M7_Body",
+		"M7_Addons"
+	};
+	hiddenSelectionsTextures[]=
+	{
+		"MA_Weapons\data\M7_SMG\Data\M7_SMG_Body_CO.paa",
+		"MA_Weapons\data\M7_SMG\Data\M7_SMG_Addons_CO.paa"
+	};
+};
+
+class 103rd_M90: 103rd_M90_SG
+{
+	scope=2;
+	scopeArsenal=2;
+	author="103rd ODST AUX";
+	model="MA_Weapons\data\M90.p3d";
+	picture="\MA_Weapons\data\M90\data\Icons\M90_Icon.paa";
+	displayName="[103rd] M90 Shotgun";
+    baseWeapon = "103rd_M90";
+	hiddenSelections[]=
+	{
+		"Camo"
+	};
+	hiddenSelectionsTextures[]=
+	{
+		"MA_Weapons\data\M90\data\M90_Shotgun_CO.paa"
+	};
+};
+
+class 103rd_MA5B_AR: 103rd_MA5B
+{
+	scope=2;
+	scopeArsenal=2;
+	author="103rd ODST AUX";
+	model="MA_Weapons\data\MA5B.p3d";
+	picture="\MA_Weapons\data\MA5B\Icons\MA5B_Icon.paa";
+	displayName="[103rd] MA5B Assault Rifle";
+    baseWeapon = "103rd_MA5B";
+	hiddenSelections[]=
+	{
+		"Camo1",
+		"Camo2"
+	};
+	hiddenSelectionsTextures[]=
+	{
+		"MA_Weapons\data\MA5B\MA5B_Weapon_co.paa",
+		"MA_Weapons\data\MA5B\MA5B_Screen_co.paa"
+	};
+};
+
+class 103rd_MA2B_AR: 103rd_MA2B
+{
+	scope=2;
+	scopeArsenal=2;
+	author="103rd ODST AUX";
+	model="MA_Weapons\data\MA_MA2B.p3d";
+	picture="\MA_Weapons\data\MA2B\data\Icons\MA2B_Icon.paa";
+	displayName="[103rd] MA2B Carbine";
+    baseWeapon = "103rd_MA2B";
+	hiddenSelections[]=
+	{
+		"Camo1"
+	};
+	hiddenSelectionsTextures[]=
+	{
+		"MA_Weapons\data\MA2B\data\MA2B_co.paa"
+	};
+};
+
+class 103rd_BR55BR: 103rd_BR55
+{
+	scope=2;
+	scopeArsenal=2;
+	author="103rd ODST AUX";
+	model="MA_Weapons\data\H2BR.p3d";
+	picture="\MA_Weapons\data\H2_BR55\Icons\BR55_Icon.paa";
+	displayName="[103rd] BR55HB Battle Rifile";
+    baseWeapon = "103rd_BR55";
+	hiddenSelections[]=
+	{
+		"Camo1",
+		"Camo2",
+		"Camo3",
+		"Camo4",
+		"Camo5"
+	};
+	hiddenSelectionsTextures[]=
+	{
+		"MA_Weapons\data\H2_BR55\BR55HB_Back_co.paa",
+		"MA_Weapons\data\H2_BR55\BR55HB_Barrel_co.paa",
+		"MA_Weapons\data\H2_BR55\BR55HB_Top_co.paa",
+		"MA_Weapons\data\H2_BR55\BR55HB_Front_co.paa",
+		"MA_Weapons\data\H2_BR55\BR55HB_Mag_co.paa"
+	};
+};
+
+class 103rd_MK50_SK: 103rd_MK50
+{
+	scope=2;
+	scopeArsenal=2;
+	author="103rd ODST AUX";
+	model="MA_Weapons\data\Sidekick.p3d";
+	picture="\MA_Weapons\data\Sidekick\Icons\Sidekick_Icon.paa";
+	displayName="[103rd] MK50 Sidekick Handgun";
+    baseWeapon = "103rd_MK50";
+	hiddenSelections[]=
+	{
+		"Camo1"
+	};
+	hiddenSelectionsTextures[]=
+	{
+		"MA_Weapons\data\Sidekick\Sidekick_CO.paa"
+	};
+};
